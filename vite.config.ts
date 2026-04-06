6c9492ea5862d3d6e4d5145d7158e7a36db1ba3e
OatMealXXII <oatbangping2@gmail.com>
2026-04-06 20:30:40 +0700

    fix: vercel build script

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,38 @@ import tailwindcss from "@tailwindcss/vite";
 import { akariMarkdownPlugin } from "./src/core/plugins/akari-md-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), vue(), akariMarkdownPlugin()],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: {
-        index: "src/index.ts",
-        plugin: "src/plugin.ts",
+export default defineConfig(({ mode }) => {
+  if (mode === "lib") {
+    return {
+      plugins: [tailwindcss(), vue(), akariMarkdownPlugin()],
+      build: {
+        copyPublicDir: false,
+        lib: {
+          entry: {
+            index: "src/index.ts",
+            plugin: "src/plugin.ts",
+          },
+          name: "AkariDocs",
+          formats: ["es"],
+          fileName: (_format, entryName) => `${entryName}.js`,
+        },
+        rollupOptions: {
+          external: [
+            "vue",
+            "vue-router",
+            "marked",
+            "lucide-vue-next",
+            "node:fs",
+            "node:path",
+            "fs",
+            "path",
+          ],
+        },
       },
-      name: "AkariDocs",
-      formats: ["es"],
-      fileName: (_format, entryName) => `${entryName}.js`,
-    },
-    rollupOptions: {
-      external: [
-        "vue",
-        "vue-router",
-        "marked",
-        "lucide-vue-next",
-        "node:fs",
-        "node:path",
-        "fs",
-        "path",
-      ],
-    },
-  },
+    };
+  }
+
+  return {
+    plugins: [tailwindcss(), vue(), akariMarkdownPlugin()],
+  };
 });
